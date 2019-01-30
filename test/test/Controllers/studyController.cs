cfdb384e0cb4ae410bb92ef0c07cdacca81cc3b6
hanjiwoo <jiw021538@gmail.com>
2019-01-31 02:47:12 +0900

    mvc_study_1-5

---
+++ test/test/Controllers/studyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using test.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace test.Controllers
+{
+    public class studyController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            // 5. 강의 내용
+            //var firstUser = new User();
+            //firstUser.UserNo = 1;
+            //firstUser.UserName = "가나다";
+
+            var firstUser = new User
+            {
+                UserNo = 1,
+                UserName = "가나다"
+            };
+
+            // 1번째 방법 : 인자값으로 
+            //return View(firstUser);
+
+            // 2번째 방법 : ViewBag
+            //ViewBag.User = firstUser;
+            //return View();
+
+            // 3번째 방법 : ViewData
+            ViewData["UserNo"] = firstUser.UserNo;
+            ViewData["UserName"] = firstUser.UserName;
+
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
